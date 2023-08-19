7bcd7425f306b3ad8863eb773ef7c8a29944bc05
Mutti <mustafa.atak@boun.edu.tr>
2023-08-19 22:35:03 +0300

    Expense App basic crud

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,31 +1,64 @@
 import { Controller, Get , Post, Delete, Put, Param, Body} from "@nestjs/common";
+import { randomUUID } from "crypto";
 
+const generateRandomId = () => {
+  let num = Math.floor(Math.random() * 1000);
+  return num.toString();
+};
 interface Report {
   name: string;
+  type: "income" | "outcome";
   idR: string;
 }
 
 let reports : Report[] = [
-  {name: 'report1', idR: "1"},
-  {name: 'report2', idR: "2"},
-  {name: 'report3', idR: "3"},
+  // {name: 'report1', type: "income", idR: generateRandomId() },
+  // {name: 'report2', type: "outcome", idR: generateRandomId() },
+  {name: 'report1', type: "income", idR: "1" },
+  {name: 'report2', type: "outcome", idR: "2" },
+  {name: 'report3', type: "income", idR: generateRandomId() },
+  {name: 'report3', type: "outcome", idR: generateRandomId() },
 ]
 
-@Controller('report')
+@Controller('reports')
 export class AppController {
 
-  @Get("/:id")
-  getReport(@Param('id') id: string) {
+  @Get()
+  getAllReports(): Report[] {
+    return reports;
+  }
 
-    let report = reports.find(report => id === report.idR);
-    return report ? report : 'not found';
+  @Get(":param")
+  getReportOrType(@Param('param') param: string) {
+    if (param === 'income' || param === 'outcome') {
+      return reports.filter(report => report.type === param);
+    } else {
+      const report = reports.find(report => param === report.idR);
+      return report ? report : 'not found';
+    }
   }
 
   @Post("/new")
-  postReport(@Body() report: Report): Report {
-    console.log(report);
-    // reports.push({name: 'report4', idR: "4"});
-    return report;
+  postReport(@Body() data: {name : string, type :  "income" | "outcome"}): Report[] {
+
+    let newIDR = generateRandomId() || randomUUID();
+    let report = { name : data.name, type : data.type, idR: newIDR};
+    reports.push(report);
+    return reports;
   }
 
+  @Put("/:id")
+  putReport(@Param('id') id: string, 
+            @Body() data: {name : string}): Report[] {
+    return reports.map(report => {
+      if(report.idR === id){
+        report.name = data.name;
+      }
+      return report})
+  }
+
+  @Delete("/:id")
+  deleteReport(@Param('id') id: string): Report[] {
+    return reports.filter(report => report.idR !== id);
+  }
 }
